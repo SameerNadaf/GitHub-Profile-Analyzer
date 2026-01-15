4fecc3478ea6bb68f2f55351f1d24a94c154a6f2
Sameer Nadaf <nsameernadaf@gmail.com>
2026-01-15 21:26:39 +0530

    Initial Commit

---
+++ GitHubProfileAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubProfileAnalyzer
+//
+//  Created by CIPL User01 on 15/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubProfileAnalyzer/GitHubProfileAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubProfileAnalyzerApp.swift
+//  GitHubProfileAnalyzer
+//
+//  Created by CIPL User01 on 15/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubProfileAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
